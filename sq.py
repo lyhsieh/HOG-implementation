369f5ec511fb3890098873bf8749c7f4a167563a
Leo Hsieh <61745234+LeoTheBestCoder@users.noreply.github.com>
2021-12-19 17:07:13 +0800

    Delete sq.py

--- sq.py
+++
@@ -1,5 +0,0 @@
-from math import sqrt
-for i in range(1, 18):
-    a = 2 ** i
-    app = 1 + (a-1)/2 + ((a-1)^2)/8 + (3*(a-1)^3)/48 + (15*(a-1)^4)/(16*24)
-    print(f"val = {a}, sqrt = {sqrt(a)}, app = {app}, diff = {abs(app - sqrt(a))}")
